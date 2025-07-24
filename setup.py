e1ec1d66de788031bdc77af813d71955d753ca9f
ted chang <htchang@us.ibm.com>
2025-07-23 18:22:58 -0700

    add build and publish gha
    Signed-off-by: ted chang <htchang@us.ibm.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="aiu-fms-testing-utils",
-    version="0.0.1",
+    use_scm_version=True,
     description="Testing Utilities for use with foundation-model-stack models running on AIU",
     packages=find_packages(),
     install_requires=["torch == 2.7.1"],
